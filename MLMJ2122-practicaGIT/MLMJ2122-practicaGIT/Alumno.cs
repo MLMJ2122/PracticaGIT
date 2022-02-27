23761bc986a88b98d1aacfcbf5ad5afa22b3824d
minga21565@iesmarenostrum.com <minga21565@iesmarenostrum.com>
2022-02-27 23:11:38 +0100

    Segunda version del proyecto, se añaden alumnos a la lista con nota

---
+++ MLMJ2122-practicaGIT/MLMJ2122-practicaGIT/Alumno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLMJ2122_practicaGIT
+{
+    class Alumno
+    {
+        private string nombre;
+        private int nota;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Nota
+        {
+            get { return nota; }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    nota = value;
+            }
+        }
+        public Boolean Aprobado
+        {
+            get
+            {
+                if (nota >= 5)
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+}
---
+++ MLMJ2122-practicaGIT/MLMJ2122-practicaGIT/Alumnos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLMJ2122_practicaGIT
+{
+    class Alumnos
+    {
+        private List<Alumno> listaAlumnos = new List<Alumno>();
+
+        // Agrega un nuevo alumno a la lista
+        //        
+        public void Agregar(Alumno alu)
+        {
+            listaAlumnos.Add(alu);
+        }
+
+        // Devuelve el alumno que está en la posición num
+        //
+        public Alumno Obtener(int num)
+        {
+            if (num >= 0 && num <= listaAlumnos.Count)
+            {
+                return ((Alumno)listaAlumnos[num]);
+            }
+            return null;
+        }
+
+        // Devuelve la nota media de los alumnos
+        //
+        public float Media
+        {
+            get
+            {
+                if (listaAlumnos.Count == 0)
+                    return 0;
+                else
+                {
+                    float media = 0;
+                    for (int i = 0; i < listaAlumnos.Count; i++)
+                    {
+                        media += ((Alumno)listaAlumnos[i]).Nota;
+                    }
+                    return (media / listaAlumnos.Count);
+                }
+            }
+        }
+    }
+}
+
